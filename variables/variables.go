dc502996171a72f6e5f42523abb190ed8d412389
Tito Yslas <roberto.yslas@gmail.com>
2019-09-09 17:46:27 -0600

    variables, mix-matched typing, declaration, scope

--- variables/variables.go
+++ variables/variables.go
@@ -2,14 +2,16 @@ package main
 
 import (
     "fmt"
-)
-
-var (
-    name, course string
-    module       float64
+    "reflect"
 )
 
 func main() {
-    fmt.Println("Name is set to", name)
-    fmt.Println("Module is set to", module)
+    name := "Tito"               //name of subscriber
+    course := "Go Fundamentals"  //course being viewed
+    module := 3.2                //current position in course
+
+    fmt.Println("Name is set to ", name, " and is type ", reflect.TypeOf(name))
+    fmt.Println("Course is set to ", course, " and is type ", reflect.TypeOf(course))
+    fmt.Println("Module is set to", module, " and is type ", reflect.TypeOf(module))
 }
+ 
