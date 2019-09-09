66d7892f3e316fb293d3364cfc4e08750182d6f5
Tito Yslas <roberto.yslas@gmail.com>
2019-09-09 17:56:40 -0600

    pointers, referencing, and de-referencing

--- variables/variables.go
+++ variables/variables.go
@@ -9,9 +9,12 @@ func main() {
     name := "Tito"               //name of subscriber
     course := "Go Fundamentals"  //course being viewed
     module := 3.2                //current position in course
+    ptr := &module               //reference to memory addres
 
     fmt.Println("Name is set to ", name, " and is type ", reflect.TypeOf(name))
     fmt.Println("Course is set to ", course, " and is type ", reflect.TypeOf(course))
     fmt.Println("Module is set to", module, " and is type ", reflect.TypeOf(module))
+    fmt.Println("Memory address of *module* variable is ", &module)
+    fmt.Println("my ptr variable value is ", ptr, " de-referencing ptr gives us ", *ptr)
 }
  
