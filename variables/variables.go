ac41f8be2c36ae374537e96a26e632d4003a60bc
Tito Yslas <roberto.yslas@gmail.com>
2019-09-09 18:05:08 -0600

    passing by value

--- variables/variables.go
+++ variables/variables.go
@@ -2,19 +2,21 @@ package main
 
 import (
     "fmt"
-    "reflect"
 )
 
 func main() {
     name := "Tito"               //name of subscriber
     course := "Go Fundamentals"  //course being viewed
-    module := 3.2                //current position in course
-    ptr := &module               //reference to memory addres
 
-    fmt.Println("Name is set to ", name, " and is type ", reflect.TypeOf(name))
-    fmt.Println("Course is set to ", course, " and is type ", reflect.TypeOf(course))
-    fmt.Println("Module is set to", module, " and is type ", reflect.TypeOf(module))
-    fmt.Println("Memory address of *module* variable is ", &module)
-    fmt.Println("my ptr variable value is ", ptr, " de-referencing ptr gives us ", *ptr)
+    fmt.Println("\nHi", name, "you're currently watching", course)
+
+    changeCourse(course)
+    fmt.Println("\nyou are now watching course ", course)
 }
  
+func changeCourse(course string) string {
+    //not a "new" variable declaration - don't need ":=" 
+    course = "Docker Deep Dive"
+    fmt.Println("\ntrying to change your course to ", course)
+    return course
+}
