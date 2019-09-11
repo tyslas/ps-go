29783570aa2e1a1ce978dc9ea48af16a59fd41eb
Tito Yslas <roberto.yslas@gmail.com>
2019-09-11 07:43:02 -0600

    break and continue

--- loops/self-destruct.go
+++ loops/self-destruct.go
@@ -8,10 +8,15 @@ import (
 func main() {
 	fmt.Println("\ninitiating count down...")
 	for timer := 10; timer >= 0; timer-- {
-		if timer == 0 {
-			fmt.Println("\nBOOM!")
-			break
+
+		// if timer == 0 {
+		// 	fmt.Println("\nBOOM!")
+		// 	break
+		// }
+		if timer % 2 == 0 {
+			continue
 		}
+
 		fmt.Println("\n", timer)
 		time.Sleep(1 * time.Second)
 	}
